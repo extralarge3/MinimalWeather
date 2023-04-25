d54bc52b84ceacfd234331d25dcf8b3e3b6726d6
extralarge3 <extralarge3@gmail.com>
2023-04-26 00:41:43 +0200

    Gitflow feature branch test

--- app/src/main/java/com/example/minimalweather/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/minimalweather/ui/main/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.minimalweather.ui.theme.MinimalWeatherTheme
 
+//git flow test
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
